abab043abd31a5b8c73606b08127d38a0b4eacd3
matstars <matstars@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-23 19:51:01 +0000

    tagging version 0.1.1
    git-svn-id: https://plugins.svn.wordpress.org/super-recent-posts-widget/tags/0.1.1@844066 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ javascripts/srpw-admin.js
@@ -0,0 +1,79 @@
+/*global srpwAjax,alert,console,jQuery,ajaxurl */
+var srpwForms, srpwSetupForms;
+jQuery(document).ready(function($){
+    srpwForms = function(){
+        $(".srpw-form").on("change", ".post-types", function(){
+            var $parent = $(this).closest('.srpw-form'),
+                $postTypesWrap = $parent.find('.post-types-wrap'),
+                $taxonomiesWrap = $parent.find('.taxonomies-wrap'),
+                $termsWrap = $parent.find('.terms-wrap'),
+                $loading = $postTypesWrap.find('.loading'),
+                postType = $(this).val(),
+                data = {
+                    action: "srpw_post_type_selected",
+                    postType: postType,
+                    srpwNonce: srpwAjax.srpwNonce
+                };
+            if (postType) {
+
+                $loading.css('display', 'block');
+                $.post(ajaxurl, data, function(response) {
+                    $taxonomiesWrap.find(".taxonomies").empty().html(response);
+                    $taxonomiesWrap.show();
+                    $termsWrap.hide();
+                    $loading.css('display', 'none');
+                });        
+            } else {
+                $taxonomiesWrap.hide();
+                $termsWrap.hide();
+            }
+        });
+        $(".srpw-form").on("change", ".taxonomies", function(){
+            var $parent = $(this).closest('.srpw-form'),
+                $termsWrap = $parent.find('.terms-wrap'),
+                $taxonomiesWrap = $parent.find('.taxonomies-wrap'),
+                taxonomy = $(this).val(),
+                $loading = $taxonomiesWrap.find('.loading'),
+                data = {
+                    action: "srpw_taxonomy_selected",
+                    taxonomy: taxonomy,
+                    srpwNonce: srpwAjax.srpwNonce
+                };
+            if (taxonomy) {
+                $loading.css('display', 'block');
+                $termsWrap.hide();
+                $.post(ajaxurl, data, function(response) {
+                    $parent.find(".terms").empty().html(response);
+                    $termsWrap.show();
+                    $loading.css('display', 'none');
+                    $termsWrap.show();
+                });   
+            } else {
+                $termsWrap.hide();
+            }
+
+        });
+    };
+    srpwSetupForms = function(){
+        $(".srpw-form").each(function(){
+            var $postTypesWrap = $(this).find('.post-types-wrap'),
+                $taxonomiesWrap = $(this).find('.taxonomies-wrap'),
+                $termsWrap = $(this).find('.terms-wrap'),
+                $postTypes = $postTypesWrap.find('.post-types'),
+                $taxonomies = $taxonomiesWrap.find('.taxonomies'),
+                $terms = $termsWrap.find('.terms');
+            if ($postTypes.val()){
+                $taxonomiesWrap.show();
+            } else {
+                $taxonomiesWrap.hide();
+            }
+            if ($taxonomies.val()){
+                $termsWrap.show();
+            } else {
+                $termsWrap.hide();
+            }            
+        });
+    };
+    srpwForms();
+    srpwSetupForms();
+});
---
+++ super-recent-posts-widget.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Super Recent Posts Widget
+Description: A prettier, filterable and more functional recent posts widget that allows you to choose a custom post type or specific taxonomy.
+Version: 0.1.1
+Author: Mat Gargano
+Author URI: http://www.matgargano.com
+*/
+
+require_once('lib/Class-super-recent-posts-widget.php');
+require_once('lib/Class-srpw-helper.php');
+require_once('lib/Class-srpw-ajax.php');
+Super_recent_posts_widget::init();
