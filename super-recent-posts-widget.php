3879746554e46b6fa8c875f2d0caee6cce0210ad
matstars <matstars@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-13 13:15:50 +0000

    tagging version 0.2.4
    git-svn-id: https://plugins.svn.wordpress.org/super-recent-posts-widget/tags/0.2.4@1088988 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- super-recent-posts-widget.php
+++ super-recent-posts-widget.php
@@ -2,7 +2,7 @@
 /*
   Plugin Name: Super Recent Posts Widget  
   Description: A prettier, filterable and more functional recent posts widget that allows you to choose a custom post type or specific taxonomy.  
-  Version: 0.2.3  
+  Version: 0.2.4  
   Author: Mat Gargano  
   Author URI: http://www.matgargano.com
 */
